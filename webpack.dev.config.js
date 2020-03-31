a8500a8022f0d02caf4f439ec9d4693fd24c05cc
Arnau Sanchez <pyarnau@gmail.com>
2020-03-31 12:01:53 +0200

    webpack: Proxy requests to dhis2 instance

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -33,11 +33,27 @@ webpackConfig.plugins = [
     })
 ];
 
+function log(req, res, opt) {
+    req.headers.Authorization = dhisConfig.authorization;
+    console.log('[PROXY]', req.method, req.url, '=>', opt.target);
+}
+
 webpackConfig.devServer = {
     contentBase: './src',
     progress: true,
     port: 8081,
-    open: false
+    open: false,
+    proxy: [
+        { path: '/api/**', target: dhisConfig.baseUrl, bypass: log },
+        { path: '/dhis-web-commons/**', target: dhisConfig.baseUrl, bypass: log },
+        { path: '/dhis-web-core-resource/**', target: dhisConfig.baseUrl, bypass: log },
+        { path: '/dhis-web-event-reports/**', target: dhisConfig.baseUrl, bypass: log },
+        { path: '/dhis-web-event-visualizer/**', target: dhisConfig.baseUrl, bypass: log },
+        { path: '/dhis-web-pivot/**', target: dhisConfig.baseUrl, bypass: log },
+        { path: '/dhis-web-visualizer/**', target: dhisConfig.baseUrl, bypass: log },
+        { path: '/dhis-web-data-visualizer/**', target: dhisConfig.baseUrl, bypass: log },
+        { path: '/dhis-web-maps/**', target: dhisConfig.baseUrl, bypass: log },
+    ],
 };
 
 module.exports = webpackConfig;
